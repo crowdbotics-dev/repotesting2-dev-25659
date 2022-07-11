2d90dbab4a4bdba5c8a9a142cdc4c73c1a28d7f6
Crowdbotics <engineering@crowdbotics.com>
2022-07-11 05:17:40 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Timert
+
+admin.site.register(Timert)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Timert(models.Model):
+    "Generated Model"
+    name23 = models.TextField()
